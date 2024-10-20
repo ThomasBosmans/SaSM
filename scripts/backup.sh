5920184ebcf30cbd3eb373f674ea038e3748fbbe
root <root@thomas-bosmans.uclllabs.be>
2024-10-20 15:19:26 +0200

    adding correct scripts for making a backup from /etc

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p /var/backups/inotify
-if echo "$1" | grep -qP "\..*\.sw(p|x|px)$" ; then
+if echo $1 | grep -qP "\..*\.sw(p|x|px)$"; then
     exit 0
 fi
-cp -p --parents "$1" /var/backups/inotify
-mv "/var/backups/inotify$1" "/var/backups/inotify$1_$(date +'%Y.%m.%d_%H:%M:%S')"
+cp -p --parents $1 /var/backups/inotify
+mv /var/backups/inotify$1 /var/backups/inotify$1_$(date +'%Y.%m.%d_%H:%M:%S')
 
